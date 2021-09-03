f56d4fa88286440ef4b503fd4b3ecf52b31c7e57
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2021-09-03 23:57:04 +0200

    [new] new library: memset (string)

---
+++ src/libs/libk/string/string.c
@@ -0,0 +1,30 @@
+/*
+	This file is part of an x86_64 hobbyist operating system called KnutOS
+	Everything is openly developed on GitHub: https://github.com/Tix3Dev/KnutOS/
+
+	Copyright (C) 2021  Yves Vollmeier <https://github.com/Tix3Dev>
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdint.h>
+#include <stddef.h>
+
+// fill a block of memory with a certain value
+void *memset(void *pointer, uint32_t value, size_t size)
+{
+	uint8_t *buffer_pointer = (uint8_t *)pointer;
+
+	for (size_t i = 0; i < size; i++)
+		*buffer_pointer++ = value;
+
+	return pointer;
+}
