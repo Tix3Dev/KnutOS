9eecf931038e185937a2a4eb7ecda43f78a25f19
Yves Vollmeier <yves.vollmeier.code@gmail.com>
2022-01-31 22:18:47 +0100

    [refactor] formatted code

--- src/libs/libk/io/io.c
+++ src/libs/libk/io/io.c
@@ -28,8 +28,8 @@ uint8_t io_inb(uint16_t port)
 {
     uint8_t ret;
     asm volatile("inb %1, %0"
-		: "=a"(ret)
-		: "Nd"(port));
+                 : "=a"(ret)
+                 : "Nd"(port));
 
     return ret;
 }
